9c15f8ccc4f80e55867c9759148258c53a70595f
Kirushonthu <kishore04072002@gmail.com>
2026-05-12 13:14:47 +0530

    main fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecipeDetails from "./componets/RecipeDetails";
-
 import Form from "./componets/login-page/Form";
 import Footer from "./home/Footer";
 import Header from "./home/Header";
 import Recipe from "./home/Recipe";
+import Home from "./home/Home";
 import About from "./componets/About";
 import Contact from "./componets/Contact";
 import Services from "./componets/Services";
@@ -14,15 +14,12 @@ import Protectedroute from "./componets/login-page/Protectedroute";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = useState(
     !!localStorage.getItem("accessToken")
   );
 
   return (
     <BrowserRouter>
-
-
       {isLoggedIn && <Header setIsLoggedIn={setIsLoggedIn} />}
       <ToastContainer
         position="top-right"
@@ -36,28 +33,23 @@ function App() {
 
         <Route
           path="/"
-          element={
-            isLoggedIn ? <Recipe /> : <Form setIsLoggedIn={setIsLoggedIn} />
-          }
+          element={isLoggedIn ? <Home /> : <Form setIsLoggedIn={setIsLoggedIn} />}
         />
 
         <Route element={<Protectedroute isLoggedIn={isLoggedIn} />}>
-
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/services" element={<Services />} />
 
+          {/* Recipe list page */}
+          <Route path="/recipes" element={<Recipe />} />
+
+          {/* Recipe detail page */}
           <Route path="/recipe/:id" element={<RecipeDetails />} />
-
-
         </Route>
 
       </Routes>
       {isLoggedIn && <Footer />}
-
-
-
-
     </BrowserRouter>
   );
 }
